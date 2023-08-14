7b4e479e278fe5f1060bc3fc3eeb1302e04a4496
Krisna Kieng <Krisna.Kieng@student.csulb.edu>
2023-08-14 02:33:00 -0700

    Created Fragments
    Set up temporary fragments for profile, calendar, home, forum and map pages. Rough setup for the front end portion of the profile.

--- app/src/main/java/com/example/exhausted/MainActivity.java
+++ app/src/main/java/com/example/exhausted/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.exhausted;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.exhausted.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.forum:
+                    replaceFragment(new ForumFragment());
+                    break;
+                case R.id.map:
+                    replaceFragment(new MapFragment());
+                    break;
+                case R.id.calendar:
+                    replaceFragment(new CalendarFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
