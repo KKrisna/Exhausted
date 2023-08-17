8c570a311436303f272c01b16713d231e985528b
Eric Truong <erictruong15@gmail.com>
2023-08-17 13:18:41 -0700

    Finished with login

--- app/src/main/java/com/example/exhausted/Register.java
+++ app/src/main/java/com/example/exhausted/Register.java
@@ -3,11 +3,13 @@ package com.example.exhausted;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,6 +29,22 @@ public class Register extends AppCompatActivity {
 
     ProgressBar progressBar;
 
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+            startActivity(intent);
+
+            finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +60,19 @@ public class Register extends AppCompatActivity {
 
         progressBar = findViewById(R.id.progressBar);
 
+        textView = findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,7 +86,6 @@ public class Register extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
-
                     return;
                 }
 
@@ -69,7 +99,7 @@ public class Register extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
-                                
+
                                 if (task.isSuccessful()) Toast.makeText(Register.this, "Account created.",
                                         Toast.LENGTH_SHORT).show();
 
