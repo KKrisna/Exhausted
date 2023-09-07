03eaceddce86b88e5912a94ee8ab8d8b88031278
Eric Truong <erictruong15@gmail.com>
2023-09-07 13:05:07 -0700

    User account deletion done

--- app/src/main/java/com/example/exhausted/MainActivity.java
+++ app/src/main/java/com/example/exhausted/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exhausted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
 
-    Button button;
+    Button buttonLogout;
+
+    Button buttonDeleteAcc;
 
     TextView textView;
 
@@ -27,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        button = findViewById(R.id.logout);
+        buttonLogout = findViewById(R.id.logout);
+
+        buttonDeleteAcc = findViewById(R.id.userDelete);
 
         textView = findViewById(R.id.user_details);
 
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         else textView.setText(user.getEmail());
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
@@ -53,6 +61,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
                 finish();
+            }
+        });
+
+        buttonDeleteAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Account successfully deleted",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseAuth.getInstance().signOut();
+
+                            Intent intent = new Intent(getApplicationContext(), Login.class);
+
+                            startActivity(intent);
+
+                            finish();
+                        }
+                    }
+                });
             }
         });
     }
