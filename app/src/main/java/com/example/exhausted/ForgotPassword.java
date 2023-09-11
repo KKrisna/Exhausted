23a0392f88b5a4091ee8fb11a9806ac9fa1c112c
Eric Truong <erictruong15@gmail.com>
2023-09-11 10:25:58 -0700

    Implemented reset password function

--- app/src/main/java/com/example/exhausted/ForgotPassword.java
+++ app/src/main/java/com/example/exhausted/ForgotPassword.java
@@ -1,12 +1,20 @@
 package com.example.exhausted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ForgotPassword extends AppCompatActivity {
@@ -14,6 +22,10 @@ public class ForgotPassword extends AppCompatActivity {
     TextInputEditText editTextEmail;
 
     Button buttonSendEmail, buttonGoBack;
+
+    FirebaseAuth mAuth;
+
+    ProgressBar progressBar;
 
     @Override
     public void onStart() {
@@ -34,11 +46,51 @@ public class ForgotPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        mAuth = FirebaseAuth.getInstance();
+
         editTextEmail = findViewById(R.id.email);
+
+        progressBar = findViewById(R.id.progressBar);
 
         buttonSendEmail = findViewById(R.id.sendEmail);
 
         buttonGoBack = findViewById(R.id.backButton);
-        
+
+        buttonGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        buttonSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                String emailAddress = String.valueOf(editTextEmail.getText());
+
+                Log.d("Debug", emailAddress);
+
+                auth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressBar.setVisibility(View.GONE);
+
+                        if (task.isSuccessful()) Toast.makeText(getApplicationContext(), "Reset email sent",
+                                Toast.LENGTH_SHORT).show();
+
+                        else Toast.makeText(ForgotPassword.this, "Reset email did not send.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
